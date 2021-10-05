2bd0767654be857823a58df89e7de970d68eb8ca
iou000 <iou000@naver.com>
2021-10-05 22:35:41 +0900

    spring-boot개발환경 구성

---
+++ src/main/java/com/weedkim/lend/LendApplication.java
@@ -0,0 +1,13 @@
+package com.weedkim.lend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weedkim/lend/LendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weedkim.lend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
