eb172a808cb328a7a087fb42fe52acd061172a2d
iou000 <iou000@naver.com>
2021-12-21 21:24:06 +0900

    env파일 추가, common/color.css 추가

--- frontend/src/service/image_uploader.js
+++ frontend/src/service/image_uploader.js
@@ -1,9 +1,9 @@
 class ImageUploader {
     async upload(file) {
-        const url = 'https://api.cloudinary.com/v1_1/diuhf2vfm/upload';
+        const url = process.env.REACT_APP_CLOUDINARY_IMAGE_URL;
         const data = new FormData();
         data.append('file', file);
-        data.append('upload_preset', 'ghh2xyun')
+        data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET_NAME)
         
         const result = await fetch( //클라우디너리에 data를 보냄.
             url,
